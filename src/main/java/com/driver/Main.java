8d64fb5da05921aedff0495b21f55d30803d6511
somuip <117620797+somuip@users.noreply.github.com>
2023-02-16 19:58:07 +0530

    First attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        // we have to use the getters and setters in this case because variable declared as private
+        obj.setName("Somuip");
+        System.out.println(obj.getName());
+    }
   
 }
